506f48158730dfc1263a27c5ea19791e66a438dc
Marcus Ribeiro <marcussilva15@outlook.com.br>
2025-08-16 02:03:17 -0300

    Add reports of all sales or per seller

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -44,7 +44,9 @@ export const getSales = async (req, res) => {
       return Object.keys(range).length ? range : null;
     };
 
-    const sortField = allowedSortFields.includes(req.query.sortField) ? req.query.sortField : "creationDate";
+    const sortField = allowedSortFields.includes(req.query.sortField)
+      ? req.query.sortField
+      : "creationDate";
 
     const sortOrder = req.query.sortOrder === "asc" ? 1 : -1;
 
@@ -63,7 +65,7 @@ export const getSales = async (req, res) => {
     const [totalSales, sales] = await Promise.all([
       MonthlySales.countDocuments(filters),
       MonthlySales.find(filters)
-        .sort({ [sortField]: sortOrder})
+        .sort({ [sortField]: sortOrder })
         .skip(parseInt(skip))
         .limit(parseInt(limit)),
     ]);
@@ -78,6 +80,92 @@ export const getSales = async (req, res) => {
       totalSales,
       totalPages: Math.ceil(totalSales / limit),
       sales,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+export const getSalesReports = async (req, res) => {
+  try {
+    const { sellerName } = req.query;
+    const matchStage = {};
+
+    if (sellerName) {
+      matchStage.sellerName = sellerName;
+    }
+
+    const report = await MonthlySales.aggregate([
+      { $match: matchStage },
+
+      {
+        $facet: {
+          totalAnnual: [
+            {
+              $group: {
+                _id: { year: "$year", sellerName: "$sellerName" },
+                totalAnnualSold: { $sum: "$totalSold" },
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                year: "$_id.year",
+                sellerName: "$_id.sellerName",
+                totalAnnualSold: { $round: ["$totalAnnualSold", 2] },
+              },
+            },
+            { $sort: { year: 1, sellerName: 1 } },
+          ],
+
+          avgMonthly: [
+            {
+              $group: {
+                _id: { year: "$year", sellerName: "$sellerName" },
+                avgMonthlySold: { $avg: "$totalSold" },
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                year: "$_id.year",
+                sellerName: "$_id.sellerName",
+                avgMonthlySold: { $round: ["$avgMonthlySold", 2] },
+              },
+            },
+            { $sort: { year: 1, sellerName: 1 } },
+          ],
+
+          bestMonth: [
+            {
+              $group: {
+                _id: {
+                  year: "$year",
+                  month: "$month",
+                  sellerName: "$sellerName",
+                },
+                totalSold: { $sum: "$totalSold" },
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                year: "$_id.year",
+                month: "$_id.month",
+                sellerName: "$_id.sellerName",
+                totalSold: { $round: ["$totalSold", 2] },
+              },
+            },
+            { $sort: { totalSold: -1 } },
+            { $limit: 1 },
+          ],
+        },
+      },
+    ]);
+
+    res.json({
+      success: true,
+      data: report[0],
     });
   } catch (error) {
     res.status(500).json({ error: "Server error" });
@@ -114,9 +202,22 @@ export const deleteSaleById = async (req, res) => {
   try {
     const sale = await MonthlySales.findByIdAndDelete(req.params.id);
     if (!sale) {
-      return res.status(404).json({ error: "Sale not found" });
+      return res.status(404).json({ error: `Sale ${req.params.id} not found` });
     }
     res.json({ message: `Sale ID ${req.params.id} deleted` });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+export const deleteAllSales = async (req, res) => {
+  try {
+    const sale = await MonthlySales.deleteMany();
+    console.log(sale);
+    if (sale.deletedCount < 1) {
+      return res.status(404).json({ error: "No sales found to delete" });
+    }
+    res.json({ message: `All sales deleted` });
   } catch (error) {
     res.status(500).json({ error: "Server error" });
   }
--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createSale, getSales, updateSale, deleteSaleById } from "../controllers/salesController.js";
+import { createSale, getSales, updateSale, deleteSaleById, getSalesReports, deleteAllSales } from "../controllers/salesController.js";
 import { saleCreateValidation, saleUpdateValidation, saleIdValidation } from "../validations/salesValidation.js";
 import { validateRequest } from "../middlewares/validateRequest.js";
 
@@ -7,7 +7,9 @@ const router = express.Router();
 
 router.post("/createSale", saleCreateValidation, validateRequest, createSale);
 router.get("/listSales", getSales);
+router.get("/generalReports", getSalesReports, validateRequest);
 router.put("/updateSale/:id", saleUpdateValidation, validateRequest, updateSale);
 router.delete("/deleteSaleById/:id", saleIdValidation, validateRequest, deleteSaleById);
+router.delete("/deleteAllSales", deleteAllSales, validateRequest);
 
 export default router;
